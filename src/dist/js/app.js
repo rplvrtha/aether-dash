acaa8987cb014561cbcce1432285038725379bc9
rplvrtha <radityaloviartha@gmail.com>
2025-10-19 23:38:33 +0700

    Refactor sidebar and navbar state management for improved clarity and functionality

--- src/dist/js/app.js
+++ src/dist/js/app.js
@@ -1,34 +1,85 @@
 class App {
   constructor() {
-    this.appName = "AetherDash";
-    this.appVersion = "3.0";
-    this.sidebarOpen = false;
-    this.isSidebarCollapsed =
-      localStorage.getItem("isSidebarCollapsed") === "true";
-    this.dropdownProfileOpen = false;
-    this.dropdownNotificationOpen = false;
-    this.isModalOpen = false;
+    this.app = {
+      name: "AetherDash",
+      version: "1.0-rc",
+    };
+    this.sideBar = {
+      isOpen: false,
+      isCollapsed: localStorage.getItem("isCollapsed") === "true",
+    };
+    this.navBar = {
+      profile: {
+        isOpen: false,
+      },
+      notifications: {
+        isOpen: false,
+        items: [
+          {
+            id: 1,
+            type: "comment",
+            user: { name: "Budi Doremi" },
+            content: "mengomentari postingan Anda.",
+            time: "5 menit yang lalu",
+          },
+          {
+            id: 2,
+            type: "user",
+            user: { name: "Jane Smith" },
+            content: "mendaftar sebagai pengguna baru.",
+            time: "1 jam yang lalu",
+          },
+          {
+            id: 3,
+            type: "server",
+            user: { name: "System Alert" },
+            content: "Server #3 mengalami overload.",
+            time: "3 jam yang lalu",
+          },
+        ],
+      },
+    };
+    this.flashMessage = [{ id: 1, text: "User baru telah mendaftar." }];
+    this.modal = {
+      isOpen: false,
+    };
+    this.isTransformed = false;
     this.activeTab = "details";
-    this.notifications = [{ id: 1, text: "User baru telah mendaftar." }];
   }
 
   init() {
-    this.$watch("isSidebarCollapsed", (val) =>
-      localStorage.setItem("isSidebarCollapsed", val)
+    this.$watch("isCollapsed", (val) =>
+      localStorage.setItem("isCollapsed", val)
     );
 
-    document.title = `Dasboard - ${this.appName}`;
+    document.title = `${this.app.name} - Dashboard`;
   }
 
   toggleSidebarCollapse() {
-    this.isSidebarCollapsed = !this.isSidebarCollapsed;
+    this.sideBar.isCollapsed = !this.sideBar.isCollapsed;
+    this.isTransformed = !this.isTransformed;
+  }
+
+  toggleNotifyOpen() {
+    this.navBar.notifications.isOpen = !this.navBar.notifications.isOpen;
+    this.navBar.profile.isOpen = false;
+  }
+
+  toggleProfileOpen() {
+    this.navBar.profile.isOpen = !this.navBar.profile.isOpen;
+    this.navBar.notifications.isOpen = false;
   }
 
   /**
    * @param {number} id
    */
   removeNotification(id) {
-    this.notifications = this.notifications.filter((n) => n.id !== id);
+    this.navBar.notifications.items = this.navBar.notifications.items.filter(
+      (n) => n.id !== id
+    );
+  }
+  removeFlash(id) {
+    this.flashMessage = this.flashMessage.filter((n) => n.id !== id);
   }
 }
 
